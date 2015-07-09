c858ba27a20e6363bce27675f679b14e0c5cd3da
uwflatlander <tschaab@gmail.com>
2015-07-08 20:07:44 -0500

    Convert fragment to programmatically added

--- app/src/main/java/com/testinprod/popularmovies/MovieGrid.java
+++ app/src/main/java/com/testinprod/popularmovies/MovieGrid.java
@@ -12,6 +12,12 @@ public class MovieGrid extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movie_grid);
+        if(savedInstanceState == null)
+        {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.flMovieGrid, new MovieGridFragment())
+                    .commit();
+        }
     }
 
 
