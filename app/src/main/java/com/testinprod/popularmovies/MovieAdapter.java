480aba98cba006a81bab1fc643f757111b5f4955
uwflatlander <tschaab@gmail.com>
2015-07-08 23:04:07 -0500

    Display results in GridView

---
+++ app/src/main/java/com/testinprod/popularmovies/MovieAdapter.java
@@ -0,0 +1,59 @@
+package com.testinprod.popularmovies;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
+import com.testinprod.popularmovies.models.Movie;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Tim on 7/8/2015.
+ */
+public class MovieAdapter extends BaseAdapter {
+    private Context mContext;
+    private ArrayList<Movie> mMovies;
+
+    public MovieAdapter(Context context, ArrayList<Movie> movies)
+    {
+        mContext = context;
+        mMovies = movies;
+    }
+    @Override
+    public int getCount() {
+        return mMovies.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ImageView movieThumb;
+        if(convertView == null)
+        {
+            LayoutInflater inflater = (LayoutInflater) parent.getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            movieThumb = (ImageView) inflater.inflate(R.layout.movie_item, parent, false);
+        }
+        else
+        {
+            movieThumb = (ImageView) convertView;
+        }
+
+        //Picasso.with(parent.getContext()).load(artist.images.get(0).url).into(artistImage);
+        Picasso.with(mContext).load(mMovies.get(position).getPosterPath()).into(movieThumb);
+        return movieThumb;
+    }
+}
--- app/src/main/java/com/testinprod/popularmovies/MovieGridFragment.java
+++ app/src/main/java/com/testinprod/popularmovies/MovieGridFragment.java
@@ -1,11 +1,11 @@
 package com.testinprod.popularmovies;
 
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.GridView;
 
 import com.testinprod.popularmovies.models.Movie;
 import com.testinprod.popularmovies.tasks.MovieDiscoverTask;
@@ -17,18 +17,11 @@ import java.util.ArrayList;
  * A placeholder fragment containing a simple view.
  */
 public class MovieGridFragment extends Fragment implements MovieDiscoverTask.MovieDiscoverTaskResults{
-    private TextView mTextView;
+    private GridView mMovieGrid;
 
     @Override
     public void handleMovieDiscoverResults(ArrayList<Movie> movies) {
-        String text = "";
-
-        for(Movie movie : movies)
-        {
-            text += movie.getTitle() + "\n";
-        }
-
-        mTextView.setText(text);
+        mMovieGrid.setAdapter(new MovieAdapter(getActivity(), movies));
     }
 
     public MovieGridFragment() {
@@ -38,8 +31,10 @@ public class MovieGridFragment extends Fragment implements MovieDiscoverTask.Mov
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
+        // TODO: Loading spinner
         View rootView = inflater.inflate(R.layout.fragment_movie_grid, container, false);
-        mTextView = (TextView) rootView.findViewById(R.id.tvHello);
+        mMovieGrid = (GridView) rootView.findViewById(R.id.gvMovies);
+
         MovieDiscoverTask popularTask = new MovieDiscoverTask(this);
         popularTask.execute("test");
         return rootView;
