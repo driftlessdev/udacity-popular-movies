efbb8aa86f1146ea7d232f15a470c4bae3965cbc
Tim Schaab <tschaab@gmail.com>
2015-07-10 15:07:56 -0500

    Add Retrofit POJO

---
+++ app/src/main/java/com/testinprod/popularmovies/models/MovieDiscovery.java
@@ -0,0 +1,98 @@
+
+package com.testinprod.popularmovies.models;
+
+import java.util.ArrayList;
+import java.util.List;
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+import org.parceler.Parcel;
+
+// Generated via http://www.jsonschema2pojo.org/
+@Parcel
+public class MovieDiscovery {
+
+    @Expose
+    private Integer page;
+    @Expose
+    private List<MovieModel> results = new ArrayList<MovieModel>();
+    @SerializedName("total_pages")
+    @Expose
+    private Integer totalPages;
+    @SerializedName("total_results")
+    @Expose
+    private Integer totalResults;
+
+    /**
+     * 
+     * @return
+     *     The page
+     */
+    public Integer getPage() {
+        return page;
+    }
+
+    /**
+     * 
+     * @param page
+     *     The page
+     */
+    public void setPage(Integer page) {
+        this.page = page;
+    }
+
+    /**
+     * 
+     * @return
+     *     The results
+     */
+    public List<MovieModel> getResults() {
+        return results;
+    }
+
+    /**
+     * 
+     * @param results
+     *     The results
+     */
+    public void setResults(List<MovieModel> results) {
+        this.results = results;
+    }
+
+    /**
+     * 
+     * @return
+     *     The totalPages
+     */
+    public Integer getTotalPages() {
+        return totalPages;
+    }
+
+    /**
+     * 
+     * @param totalPages
+     *     The total_pages
+     */
+    public void setTotalPages(Integer totalPages) {
+        this.totalPages = totalPages;
+    }
+
+    /**
+     * 
+     * @return
+     *     The totalResults
+     */
+    public Integer getTotalResults() {
+        return totalResults;
+    }
+
+    /**
+     * 
+     * @param totalResults
+     *     The total_results
+     */
+    public void setTotalResults(Integer totalResults) {
+        this.totalResults = totalResults;
+    }
+
+}
