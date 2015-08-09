f12c5b3bb3db91e10f1e7d88549e031eddcccdd0
uwflatlander <tschaab@gmail.com>
2015-08-08 22:51:58 -0500

    Add in delete, bulk insert tests

--- app/src/androidTest/java/com/testinprod/popularmovies/data/TestProvider.java
+++ app/src/androidTest/java/com/testinprod/popularmovies/data/TestProvider.java
@@ -7,12 +7,15 @@ import android.database.sqlite.SQLiteDatabase;
 import android.net.Uri;
 import android.test.AndroidTestCase;
 
+import java.math.BigInteger;
+import java.util.Random;
+
 /**
  * Created by Tim on 8/3/2015.
  */
 public class TestProvider extends AndroidTestCase {
 
-    public void deleteAllRecords()
+    public void deleteAllRecordsFromDB()
     {
         MovieDBHelper movieDBHelper = new MovieDBHelper(mContext);
         SQLiteDatabase db = movieDBHelper.getWritableDatabase();
@@ -21,10 +24,31 @@ public class TestProvider extends AndroidTestCase {
         db.close();
     }
 
+    public void deleteAllRecordsFromProvider()
+    {
+        mContext.getContentResolver().delete(
+                MovieContract.MovieEntry.CONTENT_URI,
+                null,
+                null
+        );
+
+        Cursor cursor = mContext.getContentResolver().query(
+                MovieContract.MovieEntry.CONTENT_URI,
+                null,
+                null,
+                null,
+                null
+        );
+
+        assertEquals(cursor.getCount(), 0);
+
+        cursor.close();
+    }
+
     @Override
     protected void setUp() throws Exception {
         super.setUp();
-        deleteAllRecords();
+        deleteAllRecordsFromDB();
     }
 
     public void testGetType()
@@ -100,6 +124,78 @@ public class TestProvider extends AndroidTestCase {
         cursor = mContext.getContentResolver().query(externalIdUri, null, null, null, null);
         TestUtilities.validateCursor("Update by external ID check failed", cursor, updatedValues);
 
+    }
+
+    public void testMovieDelete()
+    {
+        testMovieInserts();
+
+        TestUtilities.TestContentObserver movieObserver = TestUtilities.getTestContentObserver();
+        mContext.getContentResolver().registerContentObserver(MovieContract.MovieEntry.CONTENT_URI, true, movieObserver);
+
+        deleteAllRecordsFromProvider();
+
+        movieObserver.waitForNotificationOrFail();
+
+        mContext.getContentResolver().unregisterContentObserver(movieObserver);
+    }
+
+    private static final int BULK_INSERT_COUNT = 10;
+    private static ContentValues[] createBulkMovies(){
+        ContentValues[] movies = new ContentValues[BULK_INSERT_COUNT];
+
+        Random random = new Random();
+
+        for(int i = 0; i < BULK_INSERT_COUNT ; i++)
+        {
+            ContentValues movieValues = new ContentValues();
+            movieValues.put(MovieContract.MovieEntry.COLUMN_ADULT, random.nextInt(2));
+            movieValues.put(MovieContract.MovieEntry.COLUMN_BACKDROP_PATH, new BigInteger(130, random).toString(32));
+            movieValues.put(MovieContract.MovieEntry.COLUMN_MOVIE_ID, i);
+            movieValues.put(MovieContract.MovieEntry.COLUMN_ORIGINAL_LANGUAGE, "en");
+            movieValues.put(MovieContract.MovieEntry.COLUMN_ORIGINAL_TITLE, new BigInteger(130, random).toString(32));
+            movieValues.put(MovieContract.MovieEntry.COLUMN_OVERVIEW, new BigInteger(130, random).toString(32));
+            movieValues.put(MovieContract.MovieEntry.COLUMN_RELEASE_DATE, 1423807200);
+            movieValues.put(MovieContract.MovieEntry.COLUMN_POSTER_PATH, new BigInteger(130, random).toString(32));
+            movieValues.put(MovieContract.MovieEntry.COLUMN_POPULARITY, 17.71923d);
+            movieValues.put(MovieContract.MovieEntry.COLUMN_TITLE, new BigInteger(130, random).toString(32));
+            movieValues.put(MovieContract.MovieEntry.COLUMN_VIDEO, random.nextInt(2)); // false
+            movieValues.put(MovieContract.MovieEntry.COLUMN_VOTE_AVERAGE, random.nextFloat() * 10);
+            movieValues.put(MovieContract.MovieEntry.COLUMN_VOTE_COUNT, random.nextInt(300000));
+
+            movies[i] = movieValues;
+        }
+
+        return movies;
+    }
+
+    public void testBulkInsert()
+    {
+        ContentValues[] movies = createBulkMovies();
+
+        TestUtilities.TestContentObserver observer = TestUtilities.getTestContentObserver();
+        mContext.getContentResolver().registerContentObserver(MovieContract.MovieEntry.CONTENT_URI, true, observer);
+
+        mContext.getContentResolver().bulkInsert(MovieContract.MovieEntry.CONTENT_URI, movies);
+
+        Cursor cursor = mContext.getContentResolver().query(
+                MovieContract.MovieEntry.CONTENT_URI
+                ,null
+                ,null
+                ,null
+                ,null
+        );
+
+        assertEquals(cursor.getCount(), BULK_INSERT_COUNT);
+
+        cursor.moveToFirst();
+        for(int i = 0; i < BULK_INSERT_COUNT; i++, cursor.moveToNext())
+        {
+            ContentValues movie = movies[i];
+            TestUtilities.validateCurrentRecord("Error in bulk insert", cursor, movie);
+        }
+
+        cursor.close();
     }
 
     private void validateSingleMovie(long movieId, ContentValues values)
